492e3e7fd72bee927bb0ef63024595679beda041
Zhenglei Gao <zhenglei.gao@gmail.com>
2013-01-15 00:09:07 +0100

    add data

---
+++ R/FOCUS2006D.R
@@ -0,0 +1,12 @@
+#' @name FOCUS2006D
+#' @title Focus 2006 D
+#' @description This data set contains only one parent residual data.
+#' @docType data
+#' @usage focus2006a
+#' @format only parent substance instance, 1 column of residuals.
+#' @source Focus Kinetics
+#' @author 2012-03-05
+NULL
+## 
+## FOCUS2006D <- mkin_long_to_wide(FOCUS_2006_D)
+## save(FOCUS2006D,file='./../GitHub/KineticEval/data/FOCUS2006D.rda')
