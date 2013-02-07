5defe3f9b6f52c67a77f62ea283773d53fe94bf2
Zhenglei Gao <zhenglei.gao@gmail.com>
2013-02-07 11:44:19 +0100

    Adding test case data BCS2

---
+++ R/BCS2.r
@@ -0,0 +1,12 @@
+#' @name BCS2
+#' @title BCS2
+#' @description This data set contains one parent and 5 metabolites.
+#' @docType data
+#' @usage BCS1
+#' @format one parent and 5 metabolites, 6 column of residuals.
+#' @source BcS
+#' @author 2012-03-05
+NULL
+## 
+## FOCUS2006D <- mkin_long_to_wide(FOCUS_2006_D)
+## save(FOCUS2006D,file='./../GitHub/KineticEval/data/FOCUS2006D.rda')
