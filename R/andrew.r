d630501b9aa12355dd6321b1a91dd27f8c9e9cf4
Zhenglei Gao <zhenglei.gao@gmail.com>
2013-03-17 08:54:44 +0100

    add new data andrew

---
+++ R/andrew.r
@@ -0,0 +1,12 @@
+#' @name andrew
+#' @title Data from Andrew
+#' @description This data set contains multiple compartments.
+#' @docType data
+#' @usage andrew
+#' @format including parent substance instance, 4 column of residuals.
+#' @source Focus Kinetics
+#' @author 2012-03-05
+NULL
+## 
+## FOCUS2006D <- mkin_long_to_wide(FOCUS_2006_D)
+## save(FOCUS2006D,file='./../GitHub/KineticEval/data/FOCUS2006D.rda')
